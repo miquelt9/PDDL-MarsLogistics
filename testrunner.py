96901ddfb2d3331a08e92a9aa474aa5dab1bf73b
JCasahuga <joan.casahuga.altimiras@gmail.com>
2023-01-04 22:44:12 +0100

    Testrunner improved

--- testrunner.py
+++ testrunner.py
@@ -1,9 +1,19 @@
-import os;
+import os
+import subprocess;
 import time;
+import psutil;
 
 x = input("Escala tests? ")
 y = input("Total tests? ")
+z = input("Total Timeout? ")
 y = int(y)
+z = int(z)
+
+def kill(proc_pid):
+    process = psutil.Process(proc_pid)
+    for proc in process.children(recursive=False):
+        proc.kill()
+    process.kill()
 
 for i in range(y):
     total = i + 1
@@ -20,11 +30,25 @@ for i in range(y):
 times = []
 names = []
 
-for files in os.listdir(str(os.getcwd())):
+listdir = os.listdir(str(os.getcwd()))
+listdir = [f.lower() for f in listdir]
+sorted(listdir)
+
+for files in listdir:
     if files.endswith('.pddl') and files.__contains__("test_"):
         ini = time.perf_counter()
-        os.system('../Metric-FF/./ff -o domain_ext1.pddl -f ' + files)
-        times.append(time.perf_counter() - ini)
+        subp = subprocess.Popen('../Metric-FF/./ff -o domain_ext1.pddl -f ' + files, shell=True)
+        valid = True
+        try: 
+            subp.wait(z)
+        except subprocess.TimeoutExpired:
+            kill(subp.pid)
+            valid = False
+        if valid:
+            times.append(time.perf_counter() - ini)
+        else:
+            times.append("Overflow de més de " + str(z) + " segons")
+
         names.append(files)
 
 for i in range(len(times)):
